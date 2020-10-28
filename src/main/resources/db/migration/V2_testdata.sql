3608366a1230b70c7ee5eaf06b1b7a86786a34bf
Xhendor <rosendorsc@gmail.com>
2020-10-28 10:01:15 -0700

    ADD: flyway dependency

---
+++ src/main/resources/db/migration/V2_testdata.sql
@@ -0,0 +1,5 @@
+insert test_data into (value) values('Test1');
+insert test_data into (value) values('Test2');
+insert test_data into (value) values('Test3');
+insert test_data into (value) values('Test4');
+insert test_data into (value) values('Test5');
